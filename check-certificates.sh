e16557d3d904c96f4a806f72735f71d225ff3186
Matteo Tartara <matteo.tartara@par-tec.it>
2024-02-20 14:25:10 +0100

    add echo "---"

--- check-certificates.sh
+++ check-certificates.sh
@@ -3,23 +3,26 @@
 # https://access.redhat.com/solutions/5925951
 
 echo "################################"
-echo "## API"
-echo "# External API"
+echo "##### API"
+echo "## External API"
 oc get secret -n openshift-kube-apiserver external-loadbalancer-serving-certkey -o yaml -o=custom-columns=":.data.tls\.crt" | tail -1 | base64 -d | openssl x509 -noout -dates
-echo "# Internal API"
+echo "---------------------"
+echo "## Internal API"
 oc get secret -n openshift-kube-apiserver internal-loadbalancer-serving-certkey -o yaml -o=custom-columns=":.data.tls\.crt" | tail -1 | base64 -d | openssl x509 -noout -dates
 
 echo "################################"
-echo "## Kube Controller Manager"
-echo "# Client certificate"
+echo "##### Kube Controller Manager"
+echo "## Client certificate"
 oc get secret kube-controller-manager-client-cert-key -n openshift-kube-controller-manager -o=custom-columns=":.data.tls\.crt" | tail -1 | base64 -d | openssl x509 -noout -dates
-echo "# Server certificate"
+echo "---------------------"
+echo "## Server certificate"
 oc get secret serving-cert -n openshift-kube-controller-manager -o=custom-columns=":.data.tls\.crt" | tail -1 | base64 -d | openssl x509 -noout -dates
 
 echo "################################"
-echo "## Kube Scheduler"
-echo "# Client certificate"
+echo "##### Kube Scheduler"
+echo "## Client certificate"
 oc get secret kube-scheduler-client-cert-key -n openshift-kube-scheduler -o=custom-columns=":.data.tls\.crt" | tail -1 | base64 -d | openssl x509 -noout -dates
+echo "---------------------"
 echo "# Server certificate"
 oc get secret serving-cert -n openshift-kube-scheduler -o=custom-columns=":.data.tls\.crt" | tail -1 | base64 -d | openssl x509 -noout -dates
 
