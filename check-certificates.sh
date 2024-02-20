cb692441bad71ea78671da77b8aba786ba1ce189
Matteo Tartara <matteo.tartara@par-tec.it>
2024-02-20 15:31:54 +0100

    delete color

--- check-certificates.sh
+++ check-certificates.sh
@@ -3,8 +3,7 @@
 # https://access.redhat.com/solutions/5925951
 
 echo "################################"
-echo -e "\033[32m ##### API\33[0m"
-
+echo "##### API"
 echo "## External API"
 oc get secret -n openshift-kube-apiserver external-loadbalancer-serving-certkey -o yaml -o=custom-columns=":.data.tls\.crt" | tail -1 | base64 -d | openssl x509 -noout -dates
 echo "---------------------"
@@ -12,7 +11,7 @@ echo "## Internal API"
 oc get secret -n openshift-kube-apiserver internal-loadbalancer-serving-certkey -o yaml -o=custom-columns=":.data.tls\.crt" | tail -1 | base64 -d | openssl x509 -noout -dates
 
 echo "################################"
-echo -e "${GREEN} ##### Kube Controller Manager"
+echo  "##### Kube Controller Manager"
 
 echo "## Client certificate"
 oc get secret kube-controller-manager-client-cert-key -n openshift-kube-controller-manager -o=custom-columns=":.data.tls\.crt" | tail -1 | base64 -d | openssl x509 -noout -dates
@@ -21,7 +20,7 @@ echo "## Server certificate"
 oc get secret serving-cert -n openshift-kube-controller-manager -o=custom-columns=":.data.tls\.crt" | tail -1 | base64 -d | openssl x509 -noout -dates
 
 echo "################################"
-echo -e "${GREEN} ##### Kube Scheduler"
+echo  "##### Kube Scheduler"
 echo "## Client certificate"
 oc get secret kube-scheduler-client-cert-key -n openshift-kube-scheduler -o=custom-columns=":.data.tls\.crt" | tail -1 | base64 -d | openssl x509 -noout -dates
 echo "---------------------"
@@ -29,20 +28,20 @@ echo "# Server certificate"
 oc get secret serving-cert -n openshift-kube-scheduler -o=custom-columns=":.data.tls\.crt" | tail -1 | base64 -d | openssl x509 -noout -dates
 
 echo "################################"
-echo -e "${GREEN} ##### ETCD Certificates"
+echo  "##### ETCD Certificates"
 for master in $(oc get nodes -oname -l "node-role.kubernetes.io/master"|cut -d/ -f2); do
   echo "## Node: $master";
   echo "# etcd-peer-$master certificate";
-  oc get -n openshift-etcd secret etcd-peer-"$master" -o=custom-columns=":.data.tls\.crt" | tail -1 | base64 -d | openssl x509 -noout -dates;
+  oc get -n openshifttcd secret etcd-peer-"$master" -o=custom-columns=":.data.tls\.crt" | tail -1 | base64 -d | openssl x509 -noout -dates;
   echo "# etcd-serving-$master certificate";
-  oc get -n openshift-etcd secret etcd-serving-"$master"  -o=custom-columns=":.data.tls\.crt" | tail -1 | base64 -d | openssl x509 -noout -dates;
+  oc get -n openshifttcd secret etcd-serving-"$master"  -o=custom-columns=":.data.tls\.crt" | tail -1 | base64 -d | openssl x509 -noout -dates;
   echo "# etcd-serving-metrics-$master certificate";
-  oc get -n openshift-etcd secret etcd-serving-metrics-"$master" -o=custom-columns=":.data.tls\.crt" | tail -1 | base64 -d | openssl x509 -noout -dates;
+  oc get -n openshifttcd secret etcd-serving-metrics-"$master" -o=custom-columns=":.data.tls\.crt" | tail -1 | base64 -d | openssl x509 -noout -dates;
   echo "---------------------";
 done
 
 echo "################################"
-echo -e "${GREEN} ##### Node Certificates"
+echo  "##### Node Certificates"
 for node in $(oc get nodes -oname); do
   echo "## Node: $node";
   echo "# kubelet-client-current";
