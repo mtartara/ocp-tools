bca468202a7acdc97fa19c836117e23988c882e5
Matteo Tartara <matteo.tartara@par-tec.it>
2024-02-20 14:30:38 +0100

    add etcd

--- check-certificates.sh
+++ check-certificates.sh
@@ -26,5 +26,15 @@ echo "---------------------"
 echo "# Server certificate"
 oc get secret serving-cert -n openshift-kube-scheduler -o=custom-columns=":.data.tls\.crt" | tail -1 | base64 -d | openssl x509 -noout -dates
 
-
-## ETCD Certificates
+echo "################################"
+echo "##### ETCD Certificates"
+for master in $(oc get nodes -oname -l "node-role.kubernetes.io/master"|cut -d/ -f2); do
+  echo "## Node: $master";
+  echo "# etcd-peer-$master certificate";
+  oc get secret etcd-peer-"$master" -o=custom-columns=":.data.tls\.crt" | tail -1 | base64 -d | openssl x509 -noout -dates;
+  echo "# etcd-serving-$master certificate";
+  oc get secret etcd-serving-"$master"  -o=custom-columns=":.data.tls\.crt" | tail -1 | base64 -d | openssl x509 -noout -dates;
+  echo "# etcd-serving-metrics-$master certificate";
+  oc get secret etcd-serving-metrics-"$master" -o=custom-columns=":.data.tls\.crt" | tail -1 | base64 -d | openssl x509 -noout -dates;
+  echo "---------------------";
+done
