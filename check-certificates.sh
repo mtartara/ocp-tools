b50a25be0b4c3e80f5573fe61fd24812a1fa53ae
Matteo Tartara <matteo.tartara@par-tec.it>
2024-02-20 15:33:17 +0100

    changes

--- check-certificates.sh
+++ check-certificates.sh
@@ -32,22 +32,22 @@ echo  "##### ETCD Certificates"
 for master in $(oc get nodes -oname -l "node-role.kubernetes.io/master"|cut -d/ -f2); do
   echo "## Node: $master";
   echo "# etcd-peer-$master certificate";
-  oc get -n openshifttcd secret etcd-peer-"$master" -o=custom-columns=":.data.tls\.crt" | tail -1 | base64 -d | openssl x509 -noout -dates;
+  oc get -n openshift-etcd secret etcd-peer-"$master" -o=custom-columns=":.data.tls\.crt" | tail -1 | base64 -d | openssl x509 -noout -dates;
   echo "# etcd-serving-$master certificate";
-  oc get -n openshifttcd secret etcd-serving-"$master"  -o=custom-columns=":.data.tls\.crt" | tail -1 | base64 -d | openssl x509 -noout -dates;
+  oc get -n openshift-etcd secret etcd-serving-"$master"  -o=custom-columns=":.data.tls\.crt" | tail -1 | base64 -d | openssl x509 -noout -dates;
   echo "# etcd-serving-metrics-$master certificate";
-  oc get -n openshifttcd secret etcd-serving-metrics-"$master" -o=custom-columns=":.data.tls\.crt" | tail -1 | base64 -d | openssl x509 -noout -dates;
+  oc get -n openshift-etcd secret etcd-serving-metrics-"$master" -o=custom-columns=":.data.tls\.crt" | tail -1 | base64 -d | openssl x509 -noout -dates;
   echo "---------------------";
 done
 
 echo "################################"
 echo  "##### Node Certificates"
-for node in $(oc get nodes -oname); do
+for node in $(oc get nodes -oname|cut -d/ -f2); do
   echo "## Node: $node";
   echo "# kubelet-client-current";
-  oc debug -q "$node" -- chroot /host openssl x509 -in /var/lib/kubelet/pki/kubelet-client-current.pem -noout -dates;
+  ssh "$node" sudo openssl x509 -in /var/lib/kubelet/pki/kubelet-client-current.pem -noout -dates;
   echo "# kubelet-server-current";
-  oc debug -q "$node" -- chroot /host openssl x509 -in /var/lib/kubelet/pki/kubelet-server-current.pem -noout -dates;
+  ssh "$node" sudo openssl x509 -in /var/lib/kubelet/pki/kubelet-server-current.pem -noout -dates;
   sleep 1;
   echo "---------------------";
 done
